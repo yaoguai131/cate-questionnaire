a4a1db9868577aceee0999e2859fbea3a0c780bd
yaoguai131 <513074121@qq.com>
2018-07-09 16:56:56 +0800

    test 版本

--- src/main/java/com/cate/catequestionnaire/controller/UserController.java
+++ src/main/java/com/cate/catequestionnaire/controller/UserController.java
@@ -12,7 +12,7 @@ public class UserController {
 	@Autowired
 	ConfigTestBean configTestBean;
 	
-	@RequestMapping("/test")
+	@RequestMapping("/")
 	public String test(){
 		StringBuffer str = new StringBuffer();
 		str.append(" secret:"+configTestBean.getSecret());
